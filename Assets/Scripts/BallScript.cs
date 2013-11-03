ced152bf758da1ebca8c00efd9f88e6e6f469f76
John Hencke <zoidtek@gmail.com>
2013-11-03 14:05:44 -0500

    Version 1

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Start () {
+		rigidbody.AddForce(0, 300f, 0);
+		//this is a test
+
+	}
+}
